2d08b73ab87a8dd19fc44f218d6f7da4f39b0a0f
Kaikyu <kaikyu.lotus@gmail.com>
2021-09-16 23:20:10 +0200

    Fix compilation issue on Linux Add repository in pubspec

--- linux/web_renderer_plugin.cc
+++ linux/web_renderer_plugin.cc
@@ -20,19 +20,10 @@
 const char kChannelName[] = "flutter/windowsize";
 const char kBadArgumentsError[] = "Bad Arguments";
 const char kNoScreenError[] = "No Screen";
-const char kGetScreenListMethod[] = "getScreenList";
-const char kGetWindowInfoMethod[] = "getWindowInfo";
 const char kSetWindowFrameMethod[] = "setWindowFrame";
-const char kSetWindowMinimumSizeMethod[] = "setWindowMinimumSize";
-const char kSetWindowMaximumSizeMethod[] = "setWindowMaximumSize";
-const char kSetWindowTitleMethod[] = "setWindowTitle";
-const char ksetWindowVisibilityMethod[] = "setWindowVisibility";
-const char kGetWindowMinimumSizeMethod[] = "getWindowMinimumSize";
-const char kGetWindowMaximumSizeMethod[] = "getWindowMaximumSize";
 const char kFrameKey[] = "frame";
 const char kVisibleFrameKey[] = "visibleFrame";
 const char kScaleFactorKey[] = "scaleFactor";
-const char kScreenKey[] = "screen";
 
 struct _FlWebRendererPlugin {
   GObject parent_instance;
@@ -98,67 +89,6 @@ FlValue* make_monitor_value(GdkMonitor* monitor) {
   return fl_value_ref(value);
 }
 
-// Gets the list of current screens.
-static FlMethodResponse* get_screen_list(FlWebRendererPlugin* self) {
-  g_autoptr(FlValue) screens = fl_value_new_list();
-
-  GdkDisplay* display = get_display(self);
-  if (display == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-
-  gint n_monitors = gdk_display_get_n_monitors(display);
-  for (gint i = 0; i < n_monitors; i++) {
-    GdkMonitor* monitor = gdk_display_get_monitor(display, i);
-    fl_value_append_take(screens, make_monitor_value(monitor));
-  }
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(screens));
-}
-
-// Gets information about the Flutter window.
-static FlMethodResponse* get_window_info(FlWebRendererPlugin* self) {
-  GtkWindow* window = get_window(self);
-  if (window == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-
-  g_autoptr(FlValue) window_info = fl_value_new_map();
-
-  gint x, y, width, height;
-  gtk_window_get_position(window, &x, &y);
-  gtk_window_get_size(window, &width, &height);
-  fl_value_set_string_take(window_info, kFrameKey,
-                           make_frame_value(x, y, width, height));
-
-  // Get the monitor this window is inside, or the primary monitor if doesn't
-  // appear to be in any.
-  GdkDisplay* display = get_display(self);
-  GdkMonitor* monitor_with_window = gdk_display_get_primary_monitor(display);
-  int n_monitors = gdk_display_get_n_monitors(display);
-  for (int i = 0; i < n_monitors; i++) {
-    GdkMonitor* monitor = gdk_display_get_monitor(display, i);
-
-    GdkRectangle frame;
-    gdk_monitor_get_geometry(monitor, &frame);
-    if ((x >= frame.x && x <= frame.x + frame.width) &&
-        (y >= frame.y && y <= frame.y + frame.width)) {
-      monitor_with_window = monitor;
-      break;
-    }
-  }
-  fl_value_set_string_take(window_info, kScreenKey,
-                           make_monitor_value(monitor_with_window));
-
-  gint scale_factor = gtk_widget_get_scale_factor(GTK_WIDGET(window));
-  fl_value_set_string_take(window_info, kScaleFactorKey,
-                           fl_value_new_float(scale_factor));
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(window_info));
-}
-
 // Sets the window position and dimensions.
 static FlMethodResponse* set_window_frame(FlWebRendererPlugin* self,
                                           FlValue* args) {
@@ -183,154 +113,6 @@ static FlMethodResponse* set_window_frame(FlWebRendererPlugin* self,
                     static_cast<gint>(height));
 
   return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
-}
-
-// Send updated window geometry to GTK.
-static void update_window_geometry(FlWebRendererPlugin* self) {
-  gtk_window_set_geometry_hints(
-      get_window(self), nullptr, &self->window_geometry,
-      static_cast<GdkWindowHints>(GDK_HINT_MIN_SIZE | GDK_HINT_MAX_SIZE));
-}
-
-// Sets the window minimum size.
-static FlMethodResponse* set_window_minimum_size(FlWebRendererPlugin* self,
-                                                 FlValue* args) {
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_LIST ||
-      fl_value_get_length(args) != 2) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Expected 2-element list", nullptr));
-  }
-  double width = fl_value_get_float(fl_value_get_list_value(args, 0));
-  double height = fl_value_get_float(fl_value_get_list_value(args, 1));
-
-  if (get_window(self) == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-
-  if (width >= 0 && height >= 0) {
-    self->window_geometry.min_width = static_cast<gint>(width);
-    self->window_geometry.min_height = static_cast<gint>(height);
-  }
-
-  update_window_geometry(self);
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
-}
-
-// Sets the window maximum size.
-static FlMethodResponse* set_window_maximum_size(FlWebRendererPlugin* self,
-                                                 FlValue* args) {
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_LIST ||
-      fl_value_get_length(args) != 2) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Expected 2-element list", nullptr));
-  }
-  double width = fl_value_get_float(fl_value_get_list_value(args, 0));
-  double height = fl_value_get_float(fl_value_get_list_value(args, 1));
-
-  if (get_window(self) == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-
-  self->window_geometry.max_width = static_cast<gint>(width);
-  self->window_geometry.max_height = static_cast<gint>(height);
-
-  // Flutter uses -1 as unconstrained, GTK doesn't have an unconstrained value.
-  if (self->window_geometry.max_width < 0) {
-    self->window_geometry.max_width = G_MAXINT;
-  }
-  if (self->window_geometry.max_height < 0) {
-    self->window_geometry.max_height = G_MAXINT;
-  }
-
-  update_window_geometry(self);
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
-}
-
-// Sets the window title.
-static FlMethodResponse* set_window_title(FlWebRendererPlugin* self,
-                                          FlValue* args) {
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_STRING) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Expected string", nullptr));
-  }
-
-  GtkWindow* window = get_window(self);
-  if (window == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-  gtk_window_set_title(window, fl_value_get_string(args));
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
-}
-
-// Sets the window visibility.
-static FlMethodResponse* set_window_visible(FlWebRendererPlugin* self,
-                                          FlValue* args) {
-  if (fl_value_get_type(args) != FL_VALUE_TYPE_BOOL) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Expected bool", nullptr));
-  }
-
-  GtkWindow* window = get_window(self);
-  if (window == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-  if (fl_value_get_bool(args)) {
-    gtk_widget_show(GTK_WIDGET(window));
-  } else {
-    gtk_widget_hide(GTK_WIDGET(window));
-  }
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
-}
-
-// Gets the window minimum size.
-static FlMethodResponse* get_window_minimum_size(FlWebRendererPlugin* self) {
-  g_autoptr(FlValue) size = fl_value_new_list();
-
-  gint min_width = self->window_geometry.min_width;
-  gint min_height = self->window_geometry.min_height;
-
-  // GTK uses -1 for the requisition size (the size GTK has calculated).
-  // Report this as zero (smallest possible) so this doesn't look like Size(-1, -1).
-  if (min_width < 0) {
-    min_width = 0;
-  }
-  if (min_height < 0) {
-    min_height = 0;
-  }
-
-  fl_value_append_take(size, fl_value_new_float(min_width));
-  fl_value_append_take(size, fl_value_new_float(min_height));
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(size));
-}
-
-// Gets the window maximum size.
-static FlMethodResponse* get_window_maximum_size(FlWebRendererPlugin* self) {
-  g_autoptr(FlValue) size = fl_value_new_list();
-
-  gint max_width = self->window_geometry.max_width;
-  gint max_height = self->window_geometry.max_height;
-
-  // Flutter uses -1 as unconstrained, GTK doesn't have an unconstrained value.
-  if (max_width == G_MAXINT) {
-    max_width = -1;
-  }
-  if (max_height == G_MAXINT) {
-    max_height = -1;
-  }
-
-  fl_value_append_take(size, fl_value_new_float(max_width));
-  fl_value_append_take(size, fl_value_new_float(max_height));
-
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(size));
 }
 
 // Called when a method call is received from Flutter.
